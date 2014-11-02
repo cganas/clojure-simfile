07f06771a279836561deb11b2d051cdc13fff6bd
cganas <ganaschristopher@gmail.com>
2014-11-02 13:12:16 -0500

    Parsing of sm file tags

--- src/clojure_simfile/core.clj
+++ src/clojure_simfile/core.clj
@@ -1,4 +1,14 @@
 (ns clojure-simfile.core)
 
 (defn parse-sm [filename]
-  (slurp filename))
+  (let [contents (slurp filename)]
+    contents))
+
+
+(def tag-regex
+  #"#(.+):(.*);")
+
+(defn parse-tags [contents]
+  (let [raw-tags (re-seq tag-regex contents)
+        tag-seq (map rest raw-tags)]
+    (reduce #(assoc %1 (keyword (first %2)) (second %2)) {} tag-seq)))
